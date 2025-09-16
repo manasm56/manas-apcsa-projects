c5b34a3f1b6f5b9b378ba8a9f0998a2403e09eb2
manasm56 <121202392+manasm56@users.noreply.github.com>
2025-09-16 21:33:58 +0000

    finished project

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -1,117 +1,101 @@
-
 package robot;
 
 import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-    // Main method to make this self-contained
     public static void main(String[] args) {
-        // LEAVE THIS ALONE!!!!!!
         String worldName = "robot/finalTestWorld2024.wld";
         World.setDelay(0);
 
         Roomba cleaner = new Roomba();
         int totalBeepers = cleaner.cleanRoom(worldName, 26, 101);
         System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
     }
 
     public int cleanRoom(String worldName, int startX, int startY) {
-        // Initialize the Robot at the starting position
-        Robot roomba = new Robot(startX, startY, East, 0);
+        
         World.readWorld(worldName);
         World.setVisible(true);
 
+        Robot roomba = new Robot(startX, startY, East, 0);
+
         int totalBeepers = 0;
-        
-          
-        int area=0;
-        int moves=0;
-        
-        int largestPile=0;
-        int pileCount=0;
-        int noOfBeepersInPile=0;
-        int positionx=0;
-        int positiony=0;
-        // simple position counters
-        boolean needToCleanMore = true;
-        
-        while (needToCleanMore) {
+        int area = 0;
+        int moves = 0;
+
+        int largestPile = 0;
+        int pileCount = 0;
+        int positionx = 0;
+        int positiony = 0;
+
+        while (true) {
+           
+            area++;
+
             
-            
-            boolean processedPile = false;
+            int pileHere = 0;
             while (roomba.nextToABeeper()) {
                 roomba.pickBeeper();
                 totalBeepers++;
-                noOfBeepersInPile++;
-                if(noOfBeepersInPile > largestPile ){
-                    largestPile = noOfBeepersInPile;
-                    positionx=roomba.avenue();
-                    positiony=roomba.street();
-                }
-                processedPile = true;
+                pileHere++;
             }
-            
-            if(processedPile) {
+
+            if (pileHere > 0) {
                 pileCount++;
+                if (pileHere > largestPile) {
+                    largestPile = pileHere;
+                    positionx = roomba.avenue();
+                    positiony = roomba.street();
+                }
             }
-            roomba.move();
-            moves++;
-            area++;
+
             
-            noOfBeepersInPile=0;
-
-            while (!roomba.frontIsClear() && roomba.facingEast()) {
-                roomba.turnLeft();
-                area++;
-                 
-                moves++;
-
-
-                if(! roomba.frontIsClear()) {
-                    needToCleanMore = false;
-                    break;
-                }
+            if (roomba.frontIsClear()) {
                 roomba.move();
-                
-                
-                roomba.turnLeft();    
+
+                continue;
             }
 
-            while (!roomba.frontIsClear() && roomba.facingWest()) {
+            
+            if (roomba.facingEast()) {
                 roomba.turnLeft();
-                roomba.turnLeft();
-                roomba.turnLeft();
-                area++;
-                moves++;
+                if (roomba.frontIsClear()) {
+                    roomba.move();
 
-                if(! roomba.frontIsClear()) {
-                    needToCleanMore = false;
+                    roomba.turnLeft();
+                    continue;
+                } else {
+                    break; 
+                }
+            } else if (roomba.facingWest()) {
+                roomba.turnLeft();
+                roomba.turnLeft();
+                roomba.turnLeft();
+                if (roomba.frontIsClear()) {
+                    roomba.move();
+
+                    roomba.turnLeft();
+                    roomba.turnLeft();
+                    roomba.turnLeft();
+                    continue;
+                } else {
                     break;
                 }
-               
-                roomba.move();
-                 
-                
-                roomba.turnLeft();
-                roomba.turnLeft();
-                roomba.turnLeft();
+            } else {
+                break; 
             }
         }
-        
-        
+
+        moves = area;
         System.out.println("number of moves the roomba did is " + moves + " moves");
         System.out.println("area of room is: " + area + " square units");
-        System.out.println("largest pile has: "+ largestPile + " beepers");
-        System.out.println( "the coordinates of largest pile is:" + "(" + positionx + "," + positiony + ")" );
-        System.out.println("The number of piles is "+ pileCount + " piles" );
-        System.out.println("Percent dirty is " + ((double) pileCount * 100/area) + "%"  );
-        System.out.println("the average pile size is " + ((double)totalBeepers/pileCount));
-        
+        System.out.println("largest pile has: " + largestPile + " beepers");
+        System.out.println("the coordinates of largest pile is: (" + positionx + "," + positiony + ")");
+        System.out.println("The number of piles is " + pileCount + " piles");
+        System.out.println("Percent dirty is " + ((double) pileCount * 100 / area) + "%");
+        System.out.println("the average pile size is " + ((double) totalBeepers / pileCount));
 
         return totalBeepers;
-        
     }
-
 }
