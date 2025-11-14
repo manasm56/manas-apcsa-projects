01fcfd0d91c56128f708d7f25d606bef24c6f573
manasm56 <121202392+manasm56@users.noreply.github.com>
2025-11-14 23:08:23 +0000

    finished card class

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,21 @@
 package cards;
 
 public class Card {
+private int suite;
+private int value;
+public static String[] val= {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };  
+public static String[] suit= { "♦", "♣", "♥", "♠" }; 
 
+public Card(int suite, int value){
+this.suite=suite;
+this.value=value;
+}
+
+public int getValue(){
+    return value;
+}
+public String toString(){
+return suit[suite]+val[value];
+}
 
 }
