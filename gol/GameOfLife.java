495ab73ca7c9fab6d91f10a283f333984d441a41
manasm56 <121202392+manasm56@users.noreply.github.com>
2025-12-04 22:58:42 +0000

    synced and set up

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -9,7 +9,7 @@ public class GameOfLife implements Board {
 
     public GameOfLife(int x, int y)
     {
-        // Construct a 2d array of the given x and y size.
+        // Cnstruct a 2d array of the given x and y size.
     }
 
     // Set values  the board
