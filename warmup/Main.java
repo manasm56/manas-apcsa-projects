d14736dffa22c4bdbd657e129c30d4d2a45af43f
manasm56 <121202392+manasm56@users.noreply.github.com>
2026-01-08 22:53:31 +0000

    finish multiples of 3 & 5

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,14 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        // Your code here.
+        int a=0;
+       for (int i=0; i<1000; i++){
+        if(i%3==0 || i%5==0){
+            a+=i;
+
+        }
+       }
+         System.out.println(a);
 
     }
 }
